ccf37ab7cdc7ced074678f4aecbfa03982b14b36
Duke <dukemorise33@gmail.com>
2023-11-28 20:29:42 -0800

    completed lesson

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    evens_list = [i for i in num_list if i % 2 == 0]
+    return evens_list
+
 
 def make_exclamation(sentence_list):
-    pass
+    new_sentence_list = [sentence+"!" for sentence in sentence_list if sentence != ""]
+    return new_sentence_list
